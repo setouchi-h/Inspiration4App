8550e0c8e5d1a276afac0c202380f771cfacfa31
setouchi-h <setouchi.kazuki@gmail.com>
2024-09-23 01:05:51 +0900

    9/22

---
+++ Inspiration4App/Inspiration4App.swift
@@ -0,0 +1,18 @@
+//
+//  Inspiration4AppApp.swift
+//  Inspiration4App
+//
+//  Created by 橋本一輝 on 2024/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Inspiration4App: App {
+
+    var body: some Scene {
+        WindowGroup {
+            Areas()
+        }
+    }
+}
