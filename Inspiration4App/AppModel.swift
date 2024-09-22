186744472228be2b881c531c81a1434a619b70b4
setouchi-h <setouchi.kazuki@gmail.com>
2024-09-23 00:35:37 +0900

    Initial Commit

---
+++ Inspiration4App/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Inspiration4App
+//
+//  Created by 橋本一輝 on 2024/09/23.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Inspiration4App/Inspiration4AppApp.swift
@@ -0,0 +1,33 @@
+//
+//  Inspiration4AppApp.swift
+//  Inspiration4App
+//
+//  Created by 橋本一輝 on 2024/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Inspiration4AppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
